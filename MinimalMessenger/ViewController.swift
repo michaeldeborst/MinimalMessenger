a080cae9c72585a342a36150bf16c4bc76291d21
Michael de Borst <michael@prjct.design>
2021-08-07 17:17:46 +0300

    basic version working

--- MinimalMessenger/ViewController.swift
+++ MinimalMessenger/ViewController.swift
@@ -6,12 +6,37 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    
+    let webView = WKWebView()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.addSubview(webView)
+        
+        guard let url = URL(string: "https://web.whatsapp.com") else {
+        return
+        }
+        webView.load(URLRequest(url: url))
+        
+        DispatchQueue.main.asyncAfter(deadline: .now()+5) {
+            self.webView.evaluateJavaScript("document.body.innerHTML") {
+                 result, error in
+                guard let html = result as? String, error == nil else {
+                    return
+                }
+                print(html)
+            }
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        webView.frame = view.bounds
     }
 
 
