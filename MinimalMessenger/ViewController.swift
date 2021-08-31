d4b9cbe1f03b8e00b09c70e6ee0842d133dd63ae
Michael de Borst <michael@prjct.design>
2021-08-31 09:31:59 +0200

    Zoom-in fixed

--- MinimalMessenger/ViewController.swift
+++ MinimalMessenger/ViewController.swift
@@ -35,7 +35,11 @@ class ViewController: UIViewController {
         webView.customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_5_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15"
         
         DispatchQueue.main.asyncAfter(deadline: .now()+5) {
-            self.webView.evaluateJavaScript("document.body.innerHTML") {
+            self.webView.evaluateJavaScript("var meta = document.createElement('meta');" +
+                                                "meta.name = 'viewport';" +
+                                                "meta.content = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no';" +
+                                                "var head = document.getElementsByTagName('head')[0];" +
+                                                "head.appendChild(meta);") {
                  result, error in
                 guard let html = result as? String, error == nil else {
                     return
