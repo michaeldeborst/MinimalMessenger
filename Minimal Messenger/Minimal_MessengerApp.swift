ca7384ea5eed39a8e24c416efc015dfe273743ab
Michael de Borst <michael@prjct.design>
2021-11-26 20:56:08 +0100

    added target

---
+++ Minimal Messenger/Minimal_MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Minimal_MessengerApp.swift
+//  Minimal Messenger
+//
+//  Created by Michael de Borst on 26/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct Minimal_MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
